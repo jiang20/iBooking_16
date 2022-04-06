491c30d3da3e3ff3a6ce730ca580a17ea6479d10
jiang20 <2833306508@qq.com>
2022-04-06 13:56:31 +0800

    init

---
+++ src/main/java/com/example/ibooking_16/IBooking16Application.java
@@ -0,0 +1,13 @@
+package com.example.ibooking_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBooking16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IBooking16Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ibooking_16/IBooking16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibooking_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IBooking16ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
